5c194a4c3e06553a893ef1f6bcd3cc090df62b74
Addie <harram16@wfu.edu>
2019-09-05 15:05:59 -0400

    Commit

--- main.cpp
+++ main.cpp
@@ -10,10 +10,12 @@ int main() {
     for (i = 0; i < NUM_ELEMENTS; ++i) {
         cin >> revVctr[i];
     }
-
+    int temp = 0;
     // Reverse
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+    for (i = 0; i < (NUM_ELEMENTS / 2); ++i) {
+        temp = revVctr[i];
+        revVctr[i] = revVctr[NUM_ELEMENTS - i - 1]; // Swap
+        revVctr[NUM_ELEMENTS - i - 1] = temp;
     }
 
     // Print values
