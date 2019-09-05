4a2c37ad1e2110a35062f26349dbbec7ff1b2464
Addie <harram16@wfu.edu>
2019-09-05 15:44:06 -0400

    Commit after second task

--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];               // User values
+    vector<int> revVctr(8);               // User values
     int i;                        // Loop index
-
+    int num;
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cin >> revVctr[i];
+        cin >> revVctr.at(i);
+
     }
     int temp = 0;
     // Reverse
     for (i = 0; i < (NUM_ELEMENTS / 2); ++i) {
-        temp = revVctr[i];
-        revVctr[i] = revVctr[NUM_ELEMENTS - i - 1]; // Swap
-        revVctr[NUM_ELEMENTS - i - 1] = temp;
+        temp = revVctr.at(i);
+        revVctr.at(i) = revVctr.at(NUM_ELEMENTS - i - 1); // Swap
+        revVctr.at(NUM_ELEMENTS - i - 1) = temp;
     }
+
 
     // Print values
     cout << endl << "New values: ";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cout << " " << revVctr[i];
+        cout << " " << revVctr.at(i);
     }
     cout << endl;
 
